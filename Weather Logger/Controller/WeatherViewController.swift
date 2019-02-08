c1851f74ae2e8f08dd2360357541c984a192ca4b
Aleksandrs Muravjovs <kavalskij@gmail.com>
2019-02-08 13:16:52 +0200

    1. Added a navigation controller 2. Obtained for App ID key from openweathermap.com 3. Added CLLocationManager in to the project 4. Added all necesarry info in to info.plist in order to request user to allow gather current gps position

---
+++ Weather Logger/Controller/WeatherViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Weather Logger
+//
+//  Created by Aleksandrs Muravjovs on 08/02/2019.
+//  Copyright © 2019 Aleksandrs Muravjovs. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate  {
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "e093d3753e16fc1049bc73e08ff837f4"
+    
+    let locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    
+    
+    //MARK: - Networking
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    
+    
+}
+
